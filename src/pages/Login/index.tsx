514f6a0e841e75128dba78cf09869e083332a6f6
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-29 16:38:01 -0300

    feat: created html and css of login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,31 @@
+import { SignIn } from 'phosphor-react'
+
+import logo from '../../assets/Logo.svg'
+import login from '../../assets/Login.png'
+
+import { Container, FormLogon, ImageLogonContainer, Logo, Logon, LogonContainer } from './styles'
+
 export function Login() {
-  return <div>LOGIN</div>
+  return (
+    <Container>
+      <LogonContainer>
+        <Logo>
+          <img src={logo} alt="" />
+        </Logo>
+        <Logon>
+          <h1>Faça seu logon</h1>
+          <FormLogon action="">
+            <input type="text" placeholder="Sua ID" />
+            <button type="submit">Entrar</button>
+          </FormLogon>
+          <button>
+            <SignIn size={25} /> Não tenho cadastro
+          </button>
+        </Logon>
+      </LogonContainer>
+      <ImageLogonContainer>
+        <img src={login} alt="" />
+      </ImageLogonContainer>
+    </Container>
+  )
 }
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8.125rem;
+`
+export const LogonContainer = styled.div``
+
+export const Logo = styled.div`
+  margin-bottom: 6.25rem;
+`
+
+export const Logon = styled.div`
+  > h1 {
+    font-size: 2.25rem;
+    font-weight: 500;
+    color: ${(props) => props.theme['title-primary']};
+    margin-bottom: 2rem;
+  }
+
+  > button {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    cursor: pointer;
+    border: none;
+
+    font-size: 1.125rem;
+    font-weight: bold;
+
+    color: ${(props) => props.theme['title-secondary']};
+
+    > svg {
+      color: ${(props) => props.theme.red};
+    }
+  }
+`
+
+export const FormLogon = styled.form`
+  > input {
+    width: 22rem;
+    padding: 1.25rem 1.5rem;
+    border-radius: 8px;
+    border: 1px solid ${(props) => props.theme['input-line']};
+    color: ${(props) => props.theme['title-secondary']};
+    margin-bottom: 1rem;
+
+    ::placeholder {
+      color: ${(props) => props.theme['text-secondary']};
+    }
+  }
+
+  > button {
+    display: block;
+    width: 22rem;
+    padding: 1.25rem 0;
+    margin-bottom: 2rem;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+
+    font-size: 1.125rem;
+    font-weight: bold;
+
+    color: ${(props) => props.theme.background};
+    background-color: ${(props) => props.theme.red};
+  }
+`
+
+export const ImageLogonContainer = styled.div`
+  > img {
+    width: 100%;
+  }
+`
