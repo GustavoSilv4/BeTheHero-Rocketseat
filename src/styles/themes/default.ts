b4b8efa155ac6ae62e5c57ae36ce8526beaf559b
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-29 10:15:01 -0300

    initial commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  red: '#E02041',
+  'title-primary': '#13131A',
+  'title-secondary': '#41414D',
+  'text-primary': '#737380',
+  'text-secondary': '#A8A8B3',
+  input: '#FFFFFF',
+  'input-line': '#DCDCE6',
+  background: '#F0F0F5',
+}
