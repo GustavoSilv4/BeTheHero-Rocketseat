9ef03213b44ba817be8b2fffd9de075ee182eb50
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-07-30 19:52:19 -0300

    feat: created html and css home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,35 @@
+import logo from '../../assets/Logo.svg'
+
+import { Power } from 'phosphor-react'
+import { CasesBox, CasesContainer, Container, HeaderContainer, Logout, NavBar, NewCaseButton } from './styles'
+import { CaseCard } from '../../components/CaseCard'
+
 export function Home() {
-  return <div>HOME</div>
+  return (
+    <Container>
+      <HeaderContainer>
+        <div>
+          <img src={logo} alt="" />
+          <span>Bem vinda, APAD</span>
+        </div>
+        <NavBar>
+          <NewCaseButton>Cadastrar novo caso</NewCaseButton>
+          <Logout>
+            <Power size={24} weight="bold" />
+          </Logout>
+        </NavBar>
+      </HeaderContainer>
+
+      <CasesContainer>
+        <h1>Casos cadastrados</h1>
+        <CasesBox>
+          <CaseCard />
+          <CaseCard />
+          <CaseCard />
+          <CaseCard />
+          <CaseCard />
+        </CasesBox>
+      </CasesContainer>
+    </Container>
+  )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 2rem 10rem;
+`
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 3rem;
+
+    img {
+      width: 9rem;
+      height: 3.875rem;
+    }
+  }
+`
+
+export const NavBar = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+`
+
+export const NewCaseButton = styled.button`
+  padding: 1.3rem 3rem;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+
+  font-size: 1.125rem;
+  font-weight: bold;
+
+  color: ${(props) => props.theme.background};
+  background-color: ${(props) => props.theme.red};
+
+  :hover {
+    background-color: #d00024;
+  }
+`
+
+export const Logout = styled.button`
+  display: flex;
+  justify-content: center;
+  padding: 1.125rem;
+  border: 1px solid ${(props) => props.theme['input-line']};
+  border-radius: 8px;
+  cursor: pointer;
+
+  :hover {
+    background-color: #dedede;
+  }
+
+  svg {
+    color: ${(props) => props.theme.red};
+  }
+`
+
+export const CasesContainer = styled.main`
+  margin-top: 5rem;
+
+  > h1 {
+    font-size: 2.25rem;
+    font-weight: 500;
+
+    margin-bottom: 1.875rem;
+
+    color: ${(props) => props.theme['title-primary']};
+  }
+`
+
+export const CasesBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+`
