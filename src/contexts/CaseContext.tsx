a2da6a15263a342918f5a1e6a6c35503df8b29f8
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-08-01 18:03:33 -0300

    feat: created functions to register new case and delete a case

--- src/contexts/CaseContext.tsx
+++ src/contexts/CaseContext.tsx
@@ -1,11 +1,43 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
-export const CaseContext = createContext({})
+interface CasesProps {
+  id?: number
+  title: string
+  description: string
+  value: number
+}
+
+interface CaseContextProps {
+  registerNewCase: ({ title, description, value }: CasesProps) => void
+  deleteCase: (id: number) => void
+}
+
+export const CaseContext = createContext({} as CaseContextProps)
 
 interface CaseContextProviderProps {
   children: ReactNode
 }
-
 export function CaseContextProvider({ children }: CaseContextProviderProps) {
-  return <CaseContext.Provider value={{}}>{children}</CaseContext.Provider>
+  const [cases, setCases] = useState<CasesProps[]>([])
+
+  function registerNewCase({ title, description, value }: CasesProps) {
+    const newCase = {
+      id: new Date().getTime(),
+      title,
+      description,
+      value,
+    }
+
+    setCases((state) => [...state, newCase])
+  }
+
+  function deleteCase(id: number) {
+    const caseswithoutDeleted = cases.filter((c) => c.id !== id)
+
+    setCases(caseswithoutDeleted)
+  }
+
+  console.log('Cases', cases)
+
+  return <CaseContext.Provider value={{ registerNewCase, deleteCase }}>{children}</CaseContext.Provider>
 }
