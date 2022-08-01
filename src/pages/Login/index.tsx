d753dd4bd480af23a5cfab86a88a77b9b0ca37c5
Gustavo Silv4 <gustavosilv4.contato@gmail.com>
2022-08-01 17:26:51 -0300

    feat: created function to validate user sign in with page navigation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,9 +4,32 @@ import logo from '../../assets/Logo.svg'
 import login from '../../assets/Login.png'
 
 import { Container, FormLogon, ImageLogonContainer, Logo, Logon, LogonContainer } from './styles'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { UserContext } from '../../contexts/UserContext'
 
 export function Login() {
+  const { findUserLogin } = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  const [id, setId] = useState<number>(0)
+
+  function handleLogin(e) {
+    e.preventDefault()
+
+    if (id === 0) {
+      return
+    }
+
+    const result = findUserLogin(id)
+
+    if (result === 'ID logado com sucesso') {
+      // ** OBS: Mudar para verificacao do conteudo do objeto activeUser **
+      navigate('/home')
+    }
+  }
+
   return (
     <Container>
       <LogonContainer>
@@ -15,8 +38,8 @@ export function Login() {
         </Logo>
         <Logon>
           <h1>Faça seu logon</h1>
-          <FormLogon action="">
-            <input type="text" placeholder="Sua ID" />
+          <FormLogon action="" onSubmit={handleLogin}>
+            <input type="number" placeholder="Sua ID" onChange={(e) => setId(Number(e.target.value))} />
             <button type="submit">Entrar</button>
           </FormLogon>
           <NavLink to={'/register'}>
